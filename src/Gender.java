e988cf5c8f76468380c7cc87b7d33c61a4b15787
MaratArkhipov <maratkzn2010@yandex.ru>
2020-07-29 01:35:21 +0400

    all done

--- src/Gender.java
+++ src/Gender.java
@@ -1,4 +1,14 @@
+import java.util.List;
+import java.util.Random;
+
 public enum Gender {
     MALE,
-    FEMALE
+    FEMALE;
+    private static final List<Gender> VALUES = List.of(values());
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static Gender randomGender()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
 }
